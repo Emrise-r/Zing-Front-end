20f8263a723250d717a84fc0fe89aaa7c5c367b3
emrise <nqvinh98@gmail.com>
2020-10-16 03:31:56 +0700

    fix delete my playlist

--- src/app/songbyplaylist/songbyplaylist.component.ts
+++ src/app/songbyplaylist/songbyplaylist.component.ts
@@ -3,6 +3,9 @@ import {PlaylistSongService} from '../service/playlist-song.service';
 import {ISong} from '../interface/isong';
 import {IPlaylistSong} from '../interface/iplaylistsong';
 import {ActivatedRoute, Router} from '@angular/router';
+import {Iloginrequest} from '../interface/Iloginrequest';
+import {IplaylistService} from '../service/iplaylist.service';
+import {IPlayList} from '../interface/i-play-list';
 
 @Component({
   selector: 'app-songbyplaylist',
@@ -14,18 +17,26 @@ export class SongbyplaylistComponent implements OnInit {
   id: number;
   songs: ISong[] = [];
   iPlaylistSongs: IPlaylistSong[];
-
-  a: any[];
+  loginRequest: Iloginrequest = null;
+  playlist: IPlayList;
 
   constructor(
     private playlistSongService: PlaylistSongService,
     private activatedRoute: ActivatedRoute,
+    private getPlaylist: IplaylistService,
     private router: Router
-  ) { }
+  ) {
+    this.loginRequest = JSON.parse((sessionStorage.getItem('user')));
+    console.log(this.loginRequest.id)
+  }
 
   ngOnInit(): void {
     this.id = +this.activatedRoute.snapshot.paramMap.get('id');
     this.getAllSongByPlayList(this.id);
+    this.getPlaylist.getPlayListById(this.id).subscribe(next =>{
+      this.playlist = next;
+      console.log(this.playlist.user.userId)
+    })
     // console.log(this.iPlaylistSongs)
   }
 
@@ -40,6 +51,7 @@ export class SongbyplaylistComponent implements OnInit {
       //   this.songs.push(this.iPlaylistSongs[i].song)
       // }
       console.log(this.songs);
+      console.log(this.songs[0].user.userId);
     })
   }
 
